93cae92c952611428879c8e23b589acedf4dad0a
Arda Arslan <ardaarslan4@gmail.com>
2022-12-27 08:38:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Pazar);
+
+            int sicaklik = 32; 
+            
+            if(sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            }
+
+            else if(sicaklik>=(int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            
+            else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hadi dışarıya çıkalım.");
+            }
+    
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Sali,
+        Carsamba, 
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    
+
+    }
+
+    enum HavaDurumu
+    {
+        Soguk =5, //Derece bunlar
+        Normal=20,
+        Sicak=25,
+        CokSicak=30
+
+    }
+}
